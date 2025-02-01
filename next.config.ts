09ab9761a3e85cba5947f74bb4569ff9225dc607
omaralihussien55 <omaralihussien55@gmail.com>
2025-02-01 16:55:01 +0200

    add updatewd

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['www.91-img.com',"fdn2.gsmarena.com","fdn.gsmarena.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

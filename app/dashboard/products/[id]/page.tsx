3fac62b5629c613b1a799c864cb635f82a95540f
omaralihussien55 <omaralihussien55@gmail.com>
2025-02-01 17:53:40 +0200

    gv

--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -2,7 +2,7 @@ import ProductDetails from '@/components/products/ProductDetails'
 import React from 'react'
 
 // }
-const page = async ({params}:{params:{id:string}}) => {
+const page = async ({params}) => {
     const { id } =await params
   return (
     <div>
